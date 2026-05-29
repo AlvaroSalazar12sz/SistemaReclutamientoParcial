6013ccce4d6e4c978da0880e6404560bcc5db687
Alcachofinha <alvarosalazarzapata12sz@gmail.com>
2026-05-29 11:08:11 -0500

    modelo con las clases y dos clases requisito y rubro avanzadas

---
+++ src/main/java/com/mycompany/sistemareclutamiento/SistemaReclutamiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemareclutamiento;
+
+/**
+ *
+ * @author vayit
+ */
+public class SistemaReclutamiento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/modelo/Rubro.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Rubro{
+    private String nombre;
+    private boolean estado;
+    
+    public Rubro(){
+    }
+    public Rubro(String nombre, boolean estado){
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+    public boolean habilitar(){
+        estado = true;
+        return true;
+    }
+    public boolean deshabilitar(){
+        estado = false;
+        return true;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public boolean isEstado(){
+        return estado;
+    }
+    public void setEstado(boolean estado){
+        this.estado = estado;
+        }
+    }
